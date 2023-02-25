e2f891f4662f7142e1d8e9c5dadd1e800d5d7a08
zoneAnaS <shaikhanas4321@gmail.com>
2023-02-25 22:37:25 +0530

    this fixes #13 and #14

--- AMPCORP/src/com/ampcorp/dao/CustomerOperationsImpl.java
+++ AMPCORP/src/com/ampcorp/dao/CustomerOperationsImpl.java
@@ -17,6 +17,7 @@ import com.ampcorp.dto.BillImpl;
 import com.ampcorp.dto.Customer;
 import com.ampcorp.dto.CustomerImpl;
 import com.ampcorp.dto.Transaction;
+import com.ampcorp.dto.TransactionImpl;
 import com.ampcorp.exception.CustomerException;
 
 public class CustomerOperationsImpl implements CustomerOperations {
@@ -73,8 +74,57 @@ public class CustomerOperationsImpl implements CustomerOperations {
 
 	@Override
 	public boolean payBill(int billId) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean flag=false;
+		Connection con =DB_connection.makeConnection();
+		String PAYBILL="UPDATE BILL SET STATUS='paid' WHERE custID=? AND billDate<=?";
+		String ADDTRANS="INSERT INTO TRANSACTION(TransactionAmount,BillId,DATE_OF_TRANSACTION) VALUES(?,?,?)";
+		PreparedStatement ps;
+		try {
+			try {
+				con.setAutoCommit(false);
+				Bill bill=getUnpaidBillByBillId(billId);
+				if(bill==null) {
+					throw new CustomerException("No bill found!");
+				}
+				ps=con.prepareStatement(PAYBILL);
+				ps.setInt(1,customer.getCustomerId());
+				ps.setDate(2,Date.valueOf(bill.getBillDate()));
+				if(ps.executeUpdate()>0) {
+					ps=con.prepareStatement(ADDTRANS);
+					ps.setInt(1, bill.getTotalAmount());
+					ps.setInt(2, billId);
+					ps.setDate(3, Date.valueOf(LocalDate.now()));
+					if(ps.executeUpdate()==0) {
+						throw new CustomerException("Transaction Failed!");
+					}
+				}else {
+					throw new CustomerException("Transaction Failed!");
+				}
+				
+				
+				flag=true;
+				con.commit();
+			}catch(SQLException e) {
+				throw new CustomerException("Something went wrong!");
+			}
+			
+		}catch(CustomerException e) {
+			
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.out.println(RED+e.getMessage()+RESET);
+		}finally {
+			DB_connection.closeConnection();
+		}
+		
+		
+		
+		
+		return flag;
 	}
 
 	@Override
@@ -122,15 +172,48 @@ public class CustomerOperationsImpl implements CustomerOperations {
 
 	@Override
 	public List<Transaction> showTransactionHistory() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Transaction> list=new ArrayList<>();
+		Connection con=DB_connection.makeConnection();
+		String GET_QUERY="SELECT TransactionID,TransactionAmount,T.BillId,DATE_OF_TRANSACTION FROM Transaction T inner join Bill B on B.BillId=T.BillId Where custID=? order by DATE_OF_TRANSACTION desc";
+		
+		try {
+			try {
+				PreparedStatement ps=con.prepareStatement(GET_QUERY);
+				ps.setInt(1, customer.getCustomerId());
+				ResultSet rs=ps.executeQuery();
+				
+					while(rs.next()) {
+						int TransactionID=rs.getInt("TransactionID");
+						int TransactionAmount=rs.getInt("TransactionAmount");
+						int BillId=rs.getInt("BillId");
+						Date transDate=rs.getDate("DATE_OF_TRANSACTION");
+						SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+						String date=formatter.format(transDate);
+						Transaction trans=new TransactionImpl(BillId, TransactionAmount, LocalDate.parse(date));
+						trans.setTransactionId(TransactionID);
+						list.add(trans);
+					}
+				
+				
+				
+			}catch(SQLException  e) {
+				throw new CustomerException("Something went wrong!");
+			}
+		}catch(CustomerException e) {
+			System.out.println(RED+e.getMessage()+RESET);
+		}finally {
+			DB_connection.closeConnection();
+		}
+		
+		
+		return list;
 	}
 
 	@Override
 	public List<Bill> showAllUnpaidBill() {
 		List<Bill> billList=new ArrayList<>();
 		Connection con=null;
-		String GET_QUERY="SELECT * FROM Bill where status='unpaid' and custID=? order by billDate";
+		String GET_QUERY="SELECT * FROM Bill where status='unpaid' and custID=? order by billDate desc";
 		HashMap<Integer,Customer> map=new HashMap<>();
 		try {
 			try {
@@ -138,6 +221,7 @@ public class CustomerOperationsImpl implements CustomerOperations {
 				PreparedStatement ps=con.prepareStatement(GET_QUERY);
 				ps.setInt(1, customer.getCustomerId());
 				ResultSet rs=ps.executeQuery();
+				
 					while(rs.next()) {
 						int BillId=rs.getInt("BillId");
 						Date regDate=rs.getDate("billDate");
@@ -181,7 +265,7 @@ public class CustomerOperationsImpl implements CustomerOperations {
 	public List<Bill> showAllPaidBills() {
 		List<Bill> billList=new ArrayList<>();
 		Connection con=null;
-		String GET_QUERY="SELECT * FROM Bill where status='paid' and custID=? order by billDate";
+		String GET_QUERY="SELECT * FROM Bill where status='paid' and custID=? order by billDate desc";
 		HashMap<Integer,Customer> map=new HashMap<>();
 		try {
 			try {
@@ -189,7 +273,11 @@ public class CustomerOperationsImpl implements CustomerOperations {
 				PreparedStatement ps=con.prepareStatement(GET_QUERY);
 				ps.setInt(1, customer.getCustomerId());
 				ResultSet rs=ps.executeQuery();
+				
+				
+				
 					while(rs.next()) {
+						
 						int BillId=rs.getInt("BillId");
 						Date regDate=rs.getDate("billDate");
 						SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
@@ -267,11 +355,87 @@ public class CustomerOperationsImpl implements CustomerOperations {
 		
 		return bill;
 	}
-	
+	public Bill getUnpaidBillByBillId(int billId) {
+		Connection con=null;
+		String LOGIN_QUERY="SELECT * FROM Bill WHERE BillId=? And custID=? And status='unpaid'";
+		Bill bill=null;
+		try {
+			try {
+				con=DB_connection.makeConnection();
+				PreparedStatement ps=con.prepareStatement(LOGIN_QUERY);
+				ps.setInt(1, billId);
+				ps.setInt(2, customer.getCustomerId());
+				ResultSet rs=ps.executeQuery();
+				
+				if(rs.next()) {
+					int BillId=rs.getInt("BillId");
+					Date regDate=rs.getDate("billDate");
+					SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+					String date=formatter.format(regDate);
+					String status=rs.getString("status");
+					int units=rs.getInt("units");
+					int PreviousAmount=rs.getInt("PreviousAmount");
+					int FIXEDCHARGE=rs.getInt("FIXEDCHARGE");
+					double tax=rs.getDouble("tax");
+					int unitCharge=rs.getInt("unitCharge");
+					bill=new BillImpl(LocalDate.parse(date),units,unitCharge,PreviousAmount,FIXEDCHARGE,tax,customer);
+					bill.setBillId(BillId);
+					bill.setStatus(status);
+					
+				}else {
+					throw new CustomerException("No Bill found with given BillID for "+customer.getUserName()+"!");
+				}
+			}catch(SQLException e) {
+				throw new CustomerException("Something went wrong!");
+			}
+		}catch(CustomerException e) {
+			System.out.println(RED+e.getMessage()+RESET);
+		}
+		
+		return bill;
+	}
 	// extra methods
 	public String welcomeCustomerText(){
 		String welocameText="";
 		welocameText=GREEN+"\n******"+ANSI_BOLD+"WELCOME!!"+customer.getFirstName().toUpperCase()+" "+customer.getLastName().toUpperCase()+ANSI_RESET+GREEN+"******\n"+RESET;
 		return welocameText;
+	}
+	public Bill getLatestUnpaidBill() {
+		Connection con=null;
+		String LOGIN_QUERY="SELECT * FROM Bill WHERE custID=? And status='unpaid' Order by billDate DESC Limit 1";
+		Bill bill=null;
+		try {
+			try {
+				con=DB_connection.makeConnection();
+				PreparedStatement ps=con.prepareStatement(LOGIN_QUERY);
+				ps.setInt(1, customer.getCustomerId());
+				ResultSet rs=ps.executeQuery();
+				
+				if(rs.next()) {
+					int BillId=rs.getInt("BillId");
+					Date regDate=rs.getDate("billDate");
+					SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+					String date=formatter.format(regDate);
+					String status=rs.getString("status");
+					int units=rs.getInt("units");
+					int PreviousAmount=rs.getInt("PreviousAmount");
+					int FIXEDCHARGE=rs.getInt("FIXEDCHARGE");
+					double tax=rs.getDouble("tax");
+					int unitCharge=rs.getInt("unitCharge");
+					bill=new BillImpl(LocalDate.parse(date),units,unitCharge,PreviousAmount,FIXEDCHARGE,tax,customer);
+					bill.setBillId(BillId);
+					bill.setStatus(status);
+					
+				}else {
+					throw new CustomerException("You have paid all the bills");
+				}
+			}catch(SQLException e) {
+				throw new CustomerException("Something went wrong!");
+			}
+		}catch(CustomerException e) {
+			System.out.println(RED+e.getMessage()+RESET);
+		}
+		
+		return bill;
 	}
 }
